0d5c398ea5c4924f8483d847df16e63ed720af40
mouarius <mouarius@gmail.com>
2021-02-02 18:50:16 +0100

    Added support for Feed instead of just messages

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -5,6 +5,7 @@ import Message from './Message'
 const Chat = (props) => {
   const [inputMessage, setInputMessage] = useState('')
   const [messages, setMessages] = useState([])
+  const [feed, setFeed] = useState([])
 
   const messagesEndRef = useRef(null)
 
@@ -17,7 +18,12 @@ const Chat = (props) => {
       props.socket.on('message', (data) => {
         console.log('data :>> ', data)
         setMessages((messages) => messages.concat(data))
+        setFeed((elem) => elem.concat({ type: 'message', data: data }))
         console.log('messages :>> ', messages)
+      })
+      props.socket.on('logged user', (data) => {
+        const infoMessage = data.username + ' has logged in.'
+        setFeed((elems) => elems.concat({ type: 'info', data: infoMessage }))
       })
     }
   }, [props.socket])
@@ -40,20 +46,23 @@ const Chat = (props) => {
       className={`fixed inset-0 z-0 overflow-y-auto font-mono font-light bg-${props.color}`}
     >
       {/* <div className="min-h-screen px-4 pt-4 pb-20 text-center sm:block sm:p-0"> */}
-      <div className="inline-block overflow-hidden text-left bg-white border-4 border-m-black rounded-3xl sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
-        <header className="px-5 py-3 bg-m-black">
+      <div className="inline-block text-left bg-white border-4 border-m-black rounded-3xl sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
+        <header className="px-5 py-3 rounded-t-xl bg-m-black">
           <h1 className="text-2xl font-medium text-white">m_chat_app</h1>
         </header>
         <div className="p-5 overflow-y-scroll bg-white h-96">
-          {messages.map((message) => (
-            <Message
-              key={message.id}
-              content={message.content}
-              sender={message.sender}
-              user={props.user}
-            />
-          ))}
-          <Info message="Salut" />
+          {feed.map((element) =>
+            element.type === 'message' ? (
+              <Message
+                key={element.data.id}
+                content={element.data.content}
+                sender={element.data.sender}
+                user={props.user}
+              />
+            ) : (
+              <Info message={element.data} />
+            )
+          )}
           <div id="end-of-messages" ref={messagesEndRef} />
         </div>
         <footer className="px-5 py-3 border-t-4 border-m-black ">
--- src/components/Info.js
+++ src/components/Info.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Info = (props) => {
   const { message } = props
 
-  return <div>{message}</div>
+  return <div className="text-sm text-gray-300">{message}</div>
 }
 
 export default Info
